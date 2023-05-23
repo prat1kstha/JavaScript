a8ec408e3a06ff2db065394c244b1cfec91690f7
Pratik Shrestha <pratik.shrestha@cotiviti.com>
2023-05-23 11:02:32 +0545

    Added lecture code for operation with dates

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -16,14 +16,14 @@ const account1 = {
     pin: 1111,
 
     movementsDates: [
-        '2019-11-18T21:31:17.178Z',
-        '2019-12-23T07:42:02.383Z',
-        '2020-01-28T09:15:04.904Z',
-        '2020-04-01T10:17:24.185Z',
-        '2020-05-08T14:11:59.604Z',
-        '2020-05-27T17:01:17.194Z',
-        '2020-07-11T23:36:17.929Z',
-        '2020-07-12T10:51:36.790Z',
+        '2022-11-01T13:15:33.035Z',
+        '2022-11-30T09:48:16.867Z',
+        '2022-12-25T06:04:23.907Z',
+        '2023-01-25T14:18:46.235Z',
+        '2023-02-05T16:33:06.386Z',
+        '2023-04-10T14:43:26.374Z',
+        '2023-05-15T18:49:59.371Z',
+        '2023-05-20T12:01:20.894Z',
     ],
     currency: 'EUR',
     locale: 'pt-PT', // de-DE
@@ -36,14 +36,14 @@ const account2 = {
     pin: 2222,
 
     movementsDates: [
-        '2019-11-01T13:15:33.035Z',
-        '2019-11-30T09:48:16.867Z',
-        '2019-12-25T06:04:23.907Z',
-        '2020-01-25T14:18:46.235Z',
-        '2020-02-05T16:33:06.386Z',
-        '2020-04-10T14:43:26.374Z',
-        '2020-06-25T18:49:59.371Z',
-        '2020-07-26T12:01:20.894Z',
+        '2022-11-01T13:15:33.035Z',
+        '2022-11-30T09:48:16.867Z',
+        '2022-12-25T06:04:23.907Z',
+        '2023-01-25T14:18:46.235Z',
+        '2023-02-05T16:33:06.386Z',
+        '2023-04-10T14:43:26.374Z',
+        '2023-05-01T18:49:59.371Z',
+        '2023-05-20T12:01:20.894Z',
     ],
     currency: 'USD',
     locale: 'en-US',
@@ -81,6 +81,23 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+    const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+
+    if (daysPassed === 0) return 'Today';
+    if (daysPassed === 1) return 'Yesterday';
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+    else {
+        const year = date.getFullYear();
+        const month = `${date.getMonth() + 1}`.padStart(2, 0);
+        const day = `${date.getDate()}`.padStart(2, 0);
+        return `${day}/${month}/${year}`;
+    }
+};
+
 const displayMovements = function (acc, sort = false) {
     containerMovements.innerHTML = '';
 
@@ -92,11 +109,7 @@ const displayMovements = function (acc, sort = false) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
 
         const date = new Date(acc.movementsDates[i]);
-
-        const year = date.getFullYear();
-        const month = `${date.getMonth() + 1}`.padStart(2, 0);
-        const day = `${date.getDate()}`.padStart(2, 0);
-        const displayDate = `${day}/${month}/${year}`;
+        const displayDate = formatMovementDate(date);
 
         const html = `
       <div class="movements__row">
@@ -166,9 +179,9 @@ const updateUI = function (acc) {
 let currentAccount;
 
 //FAKE ALWAYS LOGGED IN
-// currentAccount = account1;
-// updateUI(account1);
-// containerApp.style.opacity = 100;
+currentAccount = account1;
+updateUI(account1);
+containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
     // Prevent form from submitting
@@ -352,7 +365,7 @@ btnSort.addEventListener('click', function (e) {
 // console.log(20n === 20);
 // console.log(typeof 20n);
 
-/* Creating Dates */
+// /* Creating Dates */
 // const now = new Date();
 // console.log(now);
 
@@ -361,7 +374,7 @@ btnSort.addEventListener('click', function (e) {
 
 // console.log(new Date(2023, 5, 30, 7, 41, 0));
 
-//Date from Unix Date
+// /* Date from Unix Date */
 // console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
 // /* Working with Dates */
@@ -379,3 +392,11 @@ btnSort.addEventListener('click', function (e) {
 
 // future.setFullYear('2024');
 // console.log(future);
+
+// /* Operations with Dates */
+// const future = new Date(2023, 6, 12, 19, 15, 23);
+// console.log(+future);
+
+// const calcDaysPassed = (date1, date2) =>
+//     (date2 - date1) / (1000 * 60 * 60 * 24);
+// console.log(calcDaysPassed(new Date(2023, 6, 12), new Date(2023, 6, 24)));
